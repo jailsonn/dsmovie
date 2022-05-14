dd2cb7cdbb434fd551037427a8d5f27b9790548a
jailsonn <jailsonnogueira2@gmail.com>
2022-05-14 16:53:46 -0300

    Security config

--- backend/src/main/java/com/devsuperior/dsmovie/config/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dsmovie/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+//classe de segurança de hospedagem
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
